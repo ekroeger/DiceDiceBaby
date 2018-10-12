dc222ae902d8dfffc786db44f079d7a0823760a2
Evan Kroeger <evan.kroeger@solutiondesign.com>
2018-10-12 09:50:59 -0500

    initial commit

---
+++ DiceRoller/Models/Die.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiceRoller.Models
+{
+    public class Die
+    {
+        public int Sides { get; }
+        public Die(int sides)
+        {
+            if(sides > 0)
+            {
+                this.Sides = sides;
+            }
+        }
+    }
+}
